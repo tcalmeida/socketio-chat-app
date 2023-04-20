1590a9f6121b2a5f4f4866bf751d8f7ba5e70368
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-20 09:32:06 -0300

    build: create socketio server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const server = require('http').createServer();
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log('Server running on port: 3000');
 });
