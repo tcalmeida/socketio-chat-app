cf68ae3b7f2f230e75c6a967b3232585b126eaf3
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-21 11:18:57 -0300

    build: create addEventListener to obtain chat form input value

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,13 @@
+const chatRoomForm = document.getElementById('chat-form')
 const socket = io();
 
 socket.on('msgToClient', (message) => {
     console.log(message)
 } )
+
+chatRoomForm.addEventListener ('submit', (event) => {
+    event.preventDefault();
+
+    const userMsg = document.getElementById('msg').value
+    console.log(userMsg)
+})
