bf40b0465efcd8caf9c30d33bec463c6263e5905
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-24 14:50:04 -0300

    build: add admin username to bot messages

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const formatMessage = require('./utils/formatMessage');
 
 const app = express();
 const server = http.createServer(app);
@@ -8,23 +9,28 @@ const io = new Server(server);
 
 app.use(express.static('public'));
 
+const adminText = 'SimpleBot';
+
 //when client connects
 io.on('connection', (socket) => {
-  socket.emit('msgToClient', 'Welcome! Have a nice chat!');
+  socket.emit(
+    'msgToClient',
+    formatMessage(adminText, 'Welcome! Have a nice chat!')
+  );
 
-  socket.broadcast.emit('msgToClient', 'A new user has joined');
+  socket.broadcast.emit(
+    'msgToClient',
+    formatMessage(adminText, 'A new user has joined')
+  );
 
   socket.on('disconnect', () => {
-    io.emit('msgToClient', 'User disconnected');
+    io.emit('msgToClient', formatMessage(adminText, 'User disconnected'));
   });
 
   socket.on('userChatMessage', (userMsg) => {
-    console.log(userMsg)
-    io.emit('msgToClient', userMsg)
-  })
+    io.emit('msgToClient', formatMessage('USER', userMsg));
+  });
 });
-
-
 
 server.listen(3000, () => {
   console.log('Server running on port: 3000');
