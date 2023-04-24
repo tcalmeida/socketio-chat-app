7d06ff7745e500f490fc5278f845d118090eec68
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-24 15:55:49 -0300

    build: create getUser function to acess the user in the room

--- utils/controlUser.js
+++ utils/controlUser.js
@@ -1,8 +1,12 @@
-const users = []
+const users = [];
 
-function enterUser (id, username, room) {
-const user =  {id, username, room}
+function enterUser(id, username, room) {
+  const user = { id, username, room };
 
-users.push(user);
-return user;
+  users.push(user);
+  return user;
+}
+
+function getUser (id) {
+    return users.find((user) => user.id ===id)
 }
