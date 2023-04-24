ae6bec950f5c4563bbc549fa64f421c9bb752419
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-24 18:06:51 -0300

    refactor: improve indentation

--- utils/formatMessage.js
+++ utils/formatMessage.js
@@ -1,9 +1,9 @@
 const moment = require('moment');
 
-module.exports = function formatMessage(username, text) {
+module.exports = function formatDateMessage(username, text) {
   return {
     username,
     text,
-    time: moment().format('h:mm a'),
+    time: moment().format('HH:mm:ss'),
   };
 };
