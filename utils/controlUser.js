910f9e8fa61fee5ab0deba781023edf165440acf
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-24 17:13:40 -0300

    build: create exitFunction to remove user from user array (leaves room)

--- utils/controlUser.js
+++ utils/controlUser.js
@@ -2,11 +2,22 @@ const users = [];
 
 function enterUser(id, username, room) {
   const user = { id, username, room };
-
   users.push(user);
+
   return user;
 }
 
-function getUser (id) {
-    return users.find((user) => user.id ===id)
+function getUser(id) {
+  return users.find((user) => user.id === id);
 }
+
+function exitUser() {
+  const userIndex = user.findIndex((user) => user.id === id);
+  if (userIndex !== -1) {
+    return users.splice(index, 1);
+  }
+}
+
+
+
+module.exports = { enterUser, getUser, exitUser };
