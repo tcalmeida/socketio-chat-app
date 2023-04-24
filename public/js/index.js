58cb900fd84a126521511f15415c3fb6567ea294
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-04-24 15:30:43 -0300

    build: obtain username and room values from url querystring

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,38 @@
-const chatRoomForm = document.getElementById('chat-form')
+const chatRoomForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+
+//parse url params
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+console.log(username, room);
+
 const socket = io();
 
 socket.on('msgToClient', (message) => {
-    console.log(message)
-} )
+  displayMessage(message);
 
-chatRoomForm.addEventListener ('submit', (event) => {
-    event.preventDefault();
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
 
-    const userMsg = document.getElementById('msg').value
-    console.log(userMsg)
-})
+chatRoomForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const userMsg = document.getElementById('msg').value;
+  socket.emit('userChatMessage', userMsg);
+
+  document.getElementById('msg').value = '';
+  document.getElementById('msg').focus();
+});
+
+function displayMessage(message) {
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('message');
+  newDiv.innerHTML = `
+    <p class="meta">${message.username}<span>${message.time}</span></p>
+            <p class="text">
+              ${message.text}
+            </p>`;
+  document.querySelector('.chat-messages').appendChild(newDiv);
+}
